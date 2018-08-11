e6b50e038c14af60dfec425c872a688bfa5152c9
Eduardo Geschonke <edu@jobcerto.com>
2018-08-10 23:00:49 -0300

    wip

--- src/MetableServiceProvider.php
+++ src/MetableServiceProvider.php
@@ -9,24 +9,24 @@ class MetableServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-            $this->loadMigrationsFrom(__DIR__ . '/../database/migrations/');
+            $this->loadMigrationsFrom(__DIR__ . '/database/migrations/');
         }
 
         if ( ! class_exists('CreateMetasTable')) {
             $timestamp = date('Y_m_d_His', time());
 
             $this->publishes([
-                __DIR__ . '/../database/migrations/create_metas_table.php.stub' => database_path('migrations/' . $timestamp . '_create_metas_table.php'),
+                __DIR__ . '/database/migrations/create_metas_table.php.stub' => database_path('migrations/' . $timestamp . '_create_metas_table.php'),
             ], 'migrations');
         }
 
         $this->publishes([
-            __DIR__ . '/../config/metable.php' => config_path('metable.php'),
+            __DIR__ . '/config/metable.php' => config_path('metable.php'),
         ], 'config');
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/metable.php', 'metable');
+        $this->mergeConfigFrom(__DIR__ . '/config/metable.php', 'metable');
     }
 }
