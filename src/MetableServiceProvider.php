c21dcd59a69d1ff2d600f2c98dfa450a4a5656b3
Eduardo Geschonke <edu@jobcerto.com>
2019-02-26 14:47:56 -0300

    fix database migration typo

--- src/MetableServiceProvider.php
+++ src/MetableServiceProvider.php
@@ -12,11 +12,11 @@ class MetableServiceProvider extends ServiceProvider
             $this->loadMigrationsFrom(__DIR__ . '/database/migrations/');
         }
 
-        if ( ! class_exists('CreateMetasTable')) {
+        if ( ! class_exists('CreateMetableTable')) {
             $timestamp = date('Y_m_d_His', time());
 
             $this->publishes([
-                __DIR__ . '/database/migrations/create_metas_table.php.stub' => database_path('migrations/' . $timestamp . '_create_metable_table.php'),
+                __DIR__ . '/database/migrations/create_metable_table.php.stub' => database_path('migrations/' . $timestamp . '_create_metable_table.php'),
             ], 'migrations');
         }
 
